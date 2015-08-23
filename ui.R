af5083f89a4e1f8551a1c1e3951850037ad9c5f9
Kevin Lanning <lanning@cal.berkeley.edu>
2015-08-22 20:25:57 -0400

    minor reformatting
    ...

--- ui.R
+++ ui.R
@@ -2,7 +2,6 @@ library(shiny)
 shinyUI(fluidPage( 
         titlePanel("Are you ready to do research in Computational Social Science?"), 
         h5 ("This expands on Conway's (2010) model of Computational Social Science as a Venn diagram."),
-        
        sidebarLayout( 
  
                     sidebarPanel(
@@ -11,7 +10,7 @@ shinyUI(fluidPage(
                         strong("0 = low, 5 = adequate, 9 = high"),
                         br(),
                         br(),
-                        #              sliderInput("comm", "Writing/presenting",  min = 0, max = 9, value = 5, step= 1), 
+                        # sliderInput("comm", "Writing/presenting",  min = 0, max = 9, value = 5, step= 1), 
                         sliderInput("socsci", "Social Science Expertise",  min = 0, max = 9, value = 5, step= 1),
                         sliderInput("math", "Quantitative Methods",  min = 0, max = 9, value = 5, step= 1),
                         sliderInput("hack", "Computer Science",  min = 0, max = 9, value = 5, step= 1)
@@ -22,7 +21,8 @@ shinyUI(fluidPage(
                       strong("Assessment:"),
                       tags$style(type='text/css', '#recommendation {
                                  color: red;}'), 
-                      verbatimTextOutput("recommendation")
+                      verbatimTextOutput("recommendation"),
+                      em("code is available at https://github.com/kevinlanning/shinyapp1")
                         ) 
                 ) 
         ))
